dc4735d4f69e640a5a850d27b6d3ab903fda4312
Kapil Chihla <134629561+Kapil-Chihla@users.noreply.github.com>
2024-06-22 13:05:23 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Landing } from './components/Landing';
+import { Home } from './components/Home';
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
